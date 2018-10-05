c531bb36747d91b242b2879ab55c3566889ddd00
briri <brian.riley@ucop.edu>
2018-10-05 15:25:01 -0700

    added cache/session to application.rb per omniauth instructions

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ module NuxeoOauth2
     # the framework and any gems in your application.
 
     # Required by Omniauth
+    #config.session_store :cookie_store, key: '_interslice_session'
+    #config.middleware.use ActionDispatch::Cookies # Required for all session management
     #config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
 
     # Load the Omniauth yaml
